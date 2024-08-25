6843fdbaa65e80b641cbecbf1fd55aa35685e77b
H82M19\Student <Student>
2024-08-25 11:15:39 +0700

    week1 js ex2 so sanh average scores

--- Bai2.js
+++ Bai2.js
@@ -13,10 +13,17 @@ console.log(averageDolphins);
 // 2. Compare the team's average scores to determine the winner of the competition,
 // and print it to the console. Don't forget that there can be a draw, so test for that
 // as well (draw means they have the same average score)
+let avaerageKoalas = scoreKoalas.reduce((x,y) => x + y) / scoreKoalas.length;
+averageDolphins > avaerageKoalas ? console.log(`Dolphins win (${averageDolphins} vs. ${avaerageKoalas})`) : console.log(`Koalas win (${avaerageKoalas} vs. ${averageDolphins})`);
+    
+
+
 // 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
 // team only wins if it has a higher score than the other team, and the same time a
 // score of at least 100 points. Hint: Use a logical operator to test for minimum
 // score, as well as multiple else-if blocks 
+
+
 // 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
 // both teams have the same score and both have a score greater or equal 100
 // points. Otherwise, no team wins the trophy
