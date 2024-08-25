65f5b542154f1052d23fadd4136d62cd1306d5c1
H82M19\Student <Student>
2024-08-25 10:34:10 +0700

    tinh BMI

--- Bai1.js
+++ Bai1.js
@@ -6,31 +6,20 @@
 
 // 1. Store Mark's and John's mass and height in variables
 
+
 let massMark = 78;
 let heightMark = 1.69;
 
 let massJohn = 92;
 let heightJohn = 1.95;
 
+
 // 2. Calculate both their BMIs  
 
     // BMI Mark
-bmiMark = massMark / (heightMark * heightMark);
+    bmiMark = massMark / (heightMark * heightMark);
     // BMI John
 bmiJohn = massJohn / (heightJohn * heightJohn);
-
-
-// Create a Boolean variable 'markHigherBMI' containing information about
-// whether Mark has a higher BMI than John.
-
-let markHigherBMI = bmiMark > bmiJohn;
-
-
-
-
-console.log("Test1:   " , bmiMark, bmiJohn, markHigherBMI);
-
-
 
 
 
